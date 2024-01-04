820decf4748c03b21263fd6e3c22de732f8c628c
Benjamin <owolabenjade@gmail.com>
2024-01-03 16:20:31 -0800

    aleyinju

---
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This is the 102-magic_calculation module
+"""
+
+def magic_calculation(a, b):
+        """
+            Recreates the bytecode provided
+                """
+                    return 98 + (a ** b)
