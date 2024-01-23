cc38c022e1cd5a425f9d52788172b66b16e5dde8
Benjamin <owolabenjade@gmail.com>
2024-01-23 15:20:28 -0800

    fall

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,30 +3,30 @@
 
 
 class Square:
-        """Class representing a square.
+    """Class representing a square.
 
-            Attributes:
-                    __size (int): Private instance attribute representing the size
-                                          of the square.
+    Attributes:
+        __size (int): Private instance attribute representing the size
+                        of the square.
 
-                                            """
-                                                def __init__(self, size=0):
-                                                            """Initialize a new Square instance.
+    """
+    def __init__(self, size=0):
+        """Initialize a new Square instance
 
-                                                                    Args:
-                                                                                size (int, optional): The size of the square (default is 0).
+        Args:
+            size (int, optional): The size of the square (default is 0).
 
-                                                                                        Raises:
-                                                                                                    TypeError: If size is not an integer.
-                                                                                                                ValueError: If size is less than 0.
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
 
-                                                                                                                        """
-                                                                                                                                if not isinstance(size, int):
-                                                                                                                                            raise TypeError("size must be an integer")
-                                                                                                                                                    elif size < 0:
-                                                                                                                                                                raise ValueError("size must be >= 0")
-                                                                                                                                                                        self.__size = size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-                                                                                                                                                                            def area(self):
-                                                                                                                                                                                    """Compute and return the area of the square."""
-                                                                                                                                                                                            return self.__size ** 2
+    def area(self):
+        """Compute and return the area of the square."""
+        return self.__size ** 2
