071a6b39656c344d14eb5f2fb806e33cdcd9439d
Benjamin <owolabenjade@gmail.com>
2024-01-25 16:03:37 -0800

    tanti

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    try:
+        raise TypeError("Type exception raised")
+    except TypeError as e:
+        print(e)
+
+# Example usage:
+# raise_exception()
