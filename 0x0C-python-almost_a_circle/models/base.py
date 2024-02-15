6edbb47a0c0ddd1d55758063df802aff7bcd82b1
Benjamin <owolabenjade@gmail.com>
2024-02-15 05:28:05 -0800

    close

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,7 +4,12 @@
 class Base:
     """Class documentation: Base"""
 
-    def __init__(self, width, height):
+    __nb_objects = 0
+
+    def __init__(self, id=None):
         """Method documentation: __init__"""
-        self.width = width
-        self.height = height
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
