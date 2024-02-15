3e6a8c8c0cb642c9e72473d86739d77612a6a465
Benjamin <owolabenjade@gmail.com>
2024-02-15 05:57:07 -0800

    compilcate

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,10 @@ class Rectangle(Base):
     def area(self):
         """Method documentation: area"""
         return self.width * self.height
+
+    def display(self):
+        """Method documentation: display"""
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
