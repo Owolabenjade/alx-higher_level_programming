124282d21a70feda9f7ead09cf9603b39239482e
Benjamin <owolabenjade@gmail.com>
2024-02-15 07:53:43 -0800

    moving

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Module documentation: base.py"""
+import json
 
 class Base:
     """Class documentation: Base"""
-
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -13,3 +13,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Method documentation: to_json_string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
