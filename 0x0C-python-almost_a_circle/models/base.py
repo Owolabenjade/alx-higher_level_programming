d6740e921bbebcc8cb7037d19d9c4f0f76684fbf
Benjamin <owolabenjade@gmail.com>
2024-02-15 08:07:32 -0800

    colonial

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,10 @@ class Base:
             else:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 file.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Method documentation: from_json_string"""
+        if json_string is None or json_string == '':
+            return []
+        return json.loads(json_string)
