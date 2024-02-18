0c4d15c5ca1bd3db62bcff61d1da8e8cd8a6b9e9
Benjamin <owolabenjade@gmail.com>
2024-02-18 14:09:15 -0800

    follow

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,62 +1,63 @@
 #include <Python.h>
+#include <stdio.h>
 
+/**
+ * print_python_list - Prints some basic info about Python lists
+ * @p: PyObject pointer
+ */
 void print_python_list(PyObject *p)
 {
-    PyListObject *list = (PyListObject *)p;
     Py_ssize_t size, i;
-
-    printf("[*] Python list info\n");
+    PyObject *obj;
 
     size = PyList_Size(p);
-    printf("[*] Size of the Python List = %ld\n", size);
-
-    printf("[*] Allocated = %ld\n", list->allocated);
-
+    printf("[*] Python list info\n[*] Size of the Python List = %ld\n[*] Allocated = %ld\n",
+           size, ((PyListObject *)p)->allocated);
     for (i = 0; i < size; i++)
     {
-        printf("Element %ld: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
-        if (PyBytes_Check(PyList_GetItem(p, i)))
-            print_python_bytes(PyList_GetItem(p, i));
+        obj = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(obj)->tp_name);
+        if (PyBytes_Check(obj))
+            print_python_bytes(obj);
     }
 }
 
+/**
+ * print_python_bytes - Prints some basic info about Python bytes
+ * @p: PyObject pointer
+ */
 void print_python_bytes(PyObject *p)
 {
-    PyBytesObject *bytes = (PyBytesObject *)p;
     Py_ssize_t size, i;
-    char *string;
+    char *str;
 
     printf("[.] bytes object info\n");
-
     if (!PyBytes_Check(p))
     {
         printf("  [ERROR] Invalid Bytes Object\n");
         return;
     }
-
     size = PyBytes_Size(p);
-    string = PyBytes_AsString(p);
-
-    printf("  size: %ld\n", size);
-    printf("  trying string: %s\n", string);
-
-    printf("  first 10 bytes:");
+    printf("  size: %ld\n  trying string: %s\n", size, PyBytes_AsString(p));
+    printf("  first %ld bytes: ", (size < 10 ? size + 1 : 10));
+    str = PyBytes_AsString(p);
     for (i = 0; i < size && i < 10; i++)
     {
-        printf(" %02x", (unsigned char)string[i]);
+        printf("%02x%c", str[i] & 0xff, i == 9 || i + 1 == size ? '\n' : ' ');
     }
-    printf("\n");
 }
 
+/**
+ * print_python_float - Prints some basic info about Python float objects
+ * @p: PyObject pointer
+ */
 void print_python_float(PyObject *p)
 {
     printf("[.] float object info\n");
-
     if (!PyFloat_Check(p))
     {
         printf("  [ERROR] Invalid Float Object\n");
         return;
     }
-
-    printf("  value: %f\n", PyFloat_AsDouble(p));
+    printf("  value: %f\n", ((PyFloatObject *)p)->ob_fval);
 }
