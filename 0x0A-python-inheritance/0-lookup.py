f47cfbce2aaeb30e5d47f4c2d1417c500446749f
Benjamin <owolabenjade@gmail.com>
2024-02-22 16:23:33 -0800

    meet

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,28 +5,28 @@ Module for lookup function.
 
 
 def lookup(obj):
-	"""
-	Returns the list of available attributes and methods of an object.
+"""
+Returns the list of available attributes and methods of an object.
 
-	Args:
-		obj: An object.
+Args:
+	obj: An object.
 
-	Returns:
-		List of attributes and methods.
+Returns:
+	List of attributes and methods.
 
-	Example:
-	>>> class MyClass:
-	...     def __init__(self):
-	...         self.name = "example"
-	...     def display(self):
-	...         return "Hello, " + self.name
-	>>> obj = MyClass()
-	>>> lookup(obj)
-	['__init__', 'display', 'name']
-	"""
-	return dir(obj)
+Example:
+>>> class MyClass:
+...     def __init__(self):
+...         self.name = "example"
+...     def display(self):
+...         return "Hello, " + self.name
+>>> obj = MyClass()
+>>> lookup(obj)
+['__init__', 'display', 'name']
+"""
+return dir(obj)
 
 
 if __name__ == "__main__":
-	import doctest
-	doctest.testmod()
+import doctest
+doctest.testmod()
