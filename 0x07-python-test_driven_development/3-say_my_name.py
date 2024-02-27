c89c4bd0e14a17bc354383c9875f4ce4c7fa67c0
Benjamin <owolabenjade@gmail.com>
2024-02-26 17:27:04 -0800

    bang

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 """
-Module to print a formatted name.
+This module defines a function say_my_name that prints "My name is <first name> <last name>".
+It validates that both first_name and last_name are strings.
 """
 
 def say_my_name(first_name, last_name=""):
-    """
-    Prints a formatted name.
-
-    Parameters:
-    - first_name (str): The first name.
-    - last_name (str, default: ""): The last name.
-
+    """Prints "My name is <first name> <last name>".
+    Args:
+        first_name (str): The first name.
+        last_name (str): The last name, optional.
     Raises:
-    - TypeError: If first_name or last_name is not a string.
+        TypeError: If either first_name or last_name is not a string.
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-
-    print("My name is {} {}".format(first_name, last_name))
+    print(f"My name is {first_name} {last_name}")
