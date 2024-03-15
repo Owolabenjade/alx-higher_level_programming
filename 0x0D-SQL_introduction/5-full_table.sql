c1ce9689d02ba243c782d2f8e95f48133ee64c49
Benjamin <owolabenjade@gmail.com>
2024-03-15 13:07:16 -0700

    sec

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- Print the full description of the table first_table from the database hbtn_0c_0
+-- This script uses SHOW CREATE TABLE to get the table's full description without using DESCRIBE or EXPLAIN
+
+SHOW CREATE TABLE first_table;
+
