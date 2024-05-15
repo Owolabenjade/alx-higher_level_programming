dda2cd0c7624b729447ba9b6a5ceb4ee380fcd18
Benjamin <owolabenjade@gmail.com>
2024-05-15 11:30:43 -0700

    twitter

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a request and displays only the status code of the response
-curl -so /dev/null -w "%{http_code}" "$1"
-
+# This script sends a request to a URL and displays only the status code of the response
+curl -s -o /dev/null -w "%{http_code}" "$1"
