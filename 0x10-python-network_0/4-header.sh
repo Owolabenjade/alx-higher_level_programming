b72f08899944bdc9c7dca569c8cc72c3ef92dc67
Benjamin <owolabenjade@gmail.com>
2024-05-15 11:20:48 -0700

    evolve

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a GET request with a custom header and displays the body of the response
-curl -sH "X-School-User-Id: 98" "$1"
-
+# This script sends a GET request to a URL with a custom header and displays the response body
+curl -s -H "X-School-User-Id: 98" "$1"
