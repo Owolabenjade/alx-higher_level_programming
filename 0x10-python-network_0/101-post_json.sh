f6e8eca1da7672c0ab64c00ea05acf472f2d725f
Benjamin <owolabenjade@gmail.com>
2024-05-15 11:35:09 -0700

    source

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a JSON POST request with the contents of a file
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
-
+# This script sends a JSON POST request to a URL with the contents of a file and displays the response body
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
