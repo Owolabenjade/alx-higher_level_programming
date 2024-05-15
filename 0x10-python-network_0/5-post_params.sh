bab57d49b86d34b683c78c21dda9e2941b5e3be0
Benjamin <owolabenjade@gmail.com>
2024-05-15 11:25:37 -0700

    show

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a POST request with specific parameters and displays the body of the response
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
-
+# This script sends a POST request to a URL with custom variables and displays the response body
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
