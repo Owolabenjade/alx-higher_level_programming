0f4671e8c1b2f39c1c3f66d713fbfb6adf38b897
Benjamin <owolabenjade@gmail.com>
2024-05-15 11:13:47 -0700

    Elon

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script displays all HTTP methods the server will accept
-curl -sI -X OPTIONS "$1" | grep Allow: | cut -d' ' -f2-
-
+# This script takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep "Allow:" | cut -d ' ' -f2-
