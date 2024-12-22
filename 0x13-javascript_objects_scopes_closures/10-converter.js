e70e8395b3fc5c4f51c8e7438d963b970518c39e
Benjamin <obeejay99@gmail.com>
2024-12-22 01:25:44 +0100

    done

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-	const numerals = "0123456789ABCDEF"; // For bases up to 16
-
-	return function (num) {
-		let result = "";
-		while (num > 0) {
-			result = numerals[num % base] + result; 
-			num = Math.floor(num / base);
-		}
-		return result || "0"; // Handle the case where num is 0
+	return function (number) {
+	  return number.toString(base);
 	};
-};
-
+  };
