0de8a00feaa2d3922ac801a22e047de5530085c5
Benjamin <obeejay99@gmail.com>
2024-12-23 08:39:21 +0100

    done

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,30 +1,45 @@
 #!/usr/bin/python3
 """
-This script adds the State object “Louisiana” to the database `hbtn_0e_6_usa`.
-It uses SQLAlchemy to manage database transactions. After adding the state, it prints
-the ID of the new state.
+Script that adds the State object "Louisiana"
+to the database hbtn_0e_6_usa
 """
 
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from urllib.parse import quote_plus
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database}')
-        Base.metadata.bind = engine
-
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        # Create new State object
-        new_state = State(name="Louisiana")
-        session.add(new_state)
-        session.commit()  # Commit to save the new state to the database
-
-        print(new_state.id)  # Print the ID of the new state
-
-        session.close()
-
+    # Get MySQL credentials and database name from command line arguments
+    username = sys.argv[1]
+    password = quote_plus(sys.argv[2])
+    database = sys.argv[3]
+    
+    # Create connection to database
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}',
+        pool_pre_ping=True
+    )
+    
+    # Create session factory
+    Session = sessionmaker(bind=engine)
+    
+    # Create session
+    session = Session()
+    
+    # Create new State object
+    new_state = State(name="Louisiana")
+    
+    # Add the new state to the session
+    session.add(new_state)
+    
+    # Commit the session to persist the changes
+    session.commit()
+    
+    # Print the new state's id
+    print(new_state.id)
+    
+    # Close session
+    session.close()
