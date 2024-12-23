096a5052c67ad889f0a4b9d91d4ccbc23b5df555
Benjamin <obeejay99@gmail.com>
2024-12-23 08:42:09 +0100

    done

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,28 +1,41 @@
 #!/usr/bin/python3
 """
-This script updates the name of the State object with an id of 2
-to "New Mexico" in the database `hbtn_0e_6_usa` using SQLAlchemy.
+Script that changes the name of a State object
+from the database hbtn_0e_6_usa
 """
 
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from urllib.parse import quote_plus
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database}')
-        Base.metadata.bind = engine
-
-        Session = sessionmaker(bind=engine)
-        session = Session()
-
-        # Find the state with id = 2
-        state = session.query(State).filter(State.id == 2).first()
-        if state:
-            state.name = "New Mexico"  # Change the name to "New Mexico"
-            session.commit()  # Commit the change to the database
-
-        session.close()
-
+    # Get MySQL credentials and database name from command line arguments
+    username = sys.argv[1]
+    password = quote_plus(sys.argv[2])
+    database = sys.argv[3]
+    
+    # Create connection to database
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}',
+        pool_pre_ping=True
+    )
+    
+    # Create session factory
+    Session = sessionmaker(bind=engine)
+    
+    # Create session
+    session = Session()
+    
+    # Query the State object with id = 2
+    state_to_update = session.query(State).filter(State.id == 2).first()
+    
+    # Update the name if state is found
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
+    
+    # Close session
+    session.close()
