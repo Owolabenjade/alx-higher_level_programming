67869eb9ffff513b5b2cea573ece47c3b1800fd1
Benjamin <obeejay99@gmail.com>
2024-12-23 08:29:22 +0100

    done

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python3
 """
-This script prints the first State object from the database `hbtn_0e_6_usa`.
-It uses SQLAlchemy to connect to the database and queries only the first state by its id.
-If the table is empty, it prints 'Nothing'.
+Script that prints the first State object from the database hbtn_0e_6_usa
 """
 
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from urllib.parse import quote
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database}')
-        Base.metadata.bind = engine
+    # Get MySQL username, password and database name from command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = quote(sys.argv[2])  # URL-encode the password
+    database_name = sys.argv[3]
 
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    # Create database engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                         .format(mysql_username, mysql_password, database_name))
 
-        # Query the first state in the database, ordered by id
-        state = session.query(State).order_by(State.id).first()
-        if state:
-            print(f"{state.id}: {state.name}")
-        else:
-            print("Nothing")
+    # Create session maker
+    Session = sessionmaker(bind=engine)
 
-        session.close()
+    # Create session
+    session = Session()
+
+    # Query first State object
+    first_state = session.query(State).order_by(State.id).first()
+
+    # Print the result
+    if first_state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(first_state.id, first_state.name))
+
+    # Close session
+    session.close()
 
