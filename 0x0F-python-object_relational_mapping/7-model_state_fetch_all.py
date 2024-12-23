d2573ea02014b348487e1427bda75f11c03c3ebc
Benjamin <obeejay99@gmail.com>
2024-12-23 08:21:15 +0100

    done

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,32 +1,50 @@
 #!/usr/bin/python3
 """
-This script lists all State objects from the database `hbtn_0e_6_usa`.
-It uses SQLAlchemy to query the database and prints each state in the format: <state id>: <state name>.
+Script that lists all State objects from the database hbtn_0e_6_usa
 """
 
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from urllib.parse import quote_plus
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-        # Create engine that connects to the MySQL database
-        engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost/{database}')
-        # Bind the engine to the metadata of the Base class so that the
-        # declaratives can be accessed through a DBSession instance
-        Base.metadata.bind = engine
-        DBSession = sessionmaker(bind=engine)
-        # A DBSession() instance establishes all conversations with the database
-        # and represents a "staging zone" for all the objects loaded into the
-        # database session object.
-        session = DBSession()
+    # Check if correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
 
-        # Query all states in the database
+    # Create the connection string
+    username = sys.argv[1]
+    password = quote_plus(sys.argv[2])
+    database = sys.argv[3]
+    
+    # Create engine that connects to the MySQL server
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, database
+        )
+    )
+
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session instance
+    session = Session()
+
+    try:
+        # Query all State objects and sort by id
         states = session.query(State).order_by(State.id).all()
+
+        # Display results
         for state in states:
-            print(f"{state.id}: {state.name}")
+            print("{}: {}".format(state.id, state.name))
 
+    except Exception as e:
+        print("Error occurred:", e)
+
+    finally:
+        # Close the session
         session.close()
-
