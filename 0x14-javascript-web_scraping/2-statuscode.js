4ef1f9c2d20b72aaa3133787612971ee87ed8c1a
Benjamin <obeejay99@gmail.com>
2025-02-12 00:09:07 +0100

    status

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
+
 const request = require('request');
+
 const url = process.argv[2];
-request(url, function (err, response) {
+
+request(url, (err, response) => {
   if (err) {
     console.log(err);
   } else {
-    console.log('code: ' + response.statusCode);
+    console.log(`code: ${response.statusCode}`);
   }
 });
+
