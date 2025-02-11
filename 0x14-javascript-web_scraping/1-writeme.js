8ebadbc5eda0b95a3318181e95c93a477e2b79d0
Benjamin <obeejay99@gmail.com>
2025-02-11 23:59:25 +0100

    write1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,8 +6,8 @@ const filePath = process.argv[2];
 const content = process.argv[3];
 
 fs.writeFile(filePath, content, 'utf-8', (err) => {
-	if (err) {
-		console.log(err);
-	}
+  if (err) {
+    console.log(err);
+  }
 });
 
