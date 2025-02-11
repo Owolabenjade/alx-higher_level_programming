e3f040cb497fa62e7adec9b2b32be9c259f9ab0a
Benjamin <obeejay99@gmail.com>
2025-02-12 00:25:23 +0100

    wars

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,18 +6,17 @@ const url = process.argv[2];
 
 request(url, (err, response, body) => {
   if (err) {
-    console.log(err);
+	  console.log(err);
   } else if (response.statusCode === 200) {
-    const films = JSON.parse(body);
-    let count = 0;
-    films.results.forEach((film) => {
-      if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-        count++;
-      }
-    });
-    console.log(count);
+	  const films = JSON.parse(body);
+	  let count = 0;
+	  films.results.forEach((film) => {
+		  if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+			  count++;
+		  }
+	  });
+	  console.log(count);
   } else {
-    console.log(`Error: ${response.statusCode}`);
+	  console.log(`Error: ${response.statusCode}`);
   }
 });
-
