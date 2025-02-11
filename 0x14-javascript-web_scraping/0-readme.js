a6ec4f1782eefe3be44e69bbee736aa60c15d0bb
Benjamin <obeejay99@gmail.com>
2025-02-11 23:50:27 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const file = process.argv[2];
-fs.readFile(file, 'utf-8', function (err, data) {
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
