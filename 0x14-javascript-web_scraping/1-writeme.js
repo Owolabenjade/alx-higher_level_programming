57948a0129d42418dda7a8744844e80fbe5b17b1
Benjamin <obeejay99@gmail.com>
2025-02-11 23:53:45 +0100

    writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const file = process.argv[2];
+
+const filePath = process.argv[2];
 const content = process.argv[3];
-fs.writeFile(file, content, 'utf-8', function (err) {
-  if (err) {
-    console.log(err);
-  }
+
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+	  if (err) {
+		      console.log(err);
+		    }
 });
+
