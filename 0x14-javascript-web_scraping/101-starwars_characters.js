696c64475d7b9d3c9390c4435ddb1da5b3a5bc38
Benjamin <obeejay99@gmail.com>
2025-02-12 01:35:36 +0100

    101wars

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,28 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+
+    characters.forEach((characterUrl) => {
+      request(characterUrl, (err, response, body) => {
+        if (err) {
+          console.log(err);
+        } else if (response.statusCode === 200) {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    });
+  } else {
+    console.log(`Error: ${response.statusCode}`);
   }
 });
-
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
-  });
-}
